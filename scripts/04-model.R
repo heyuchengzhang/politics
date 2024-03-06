51573211f9e9ace6d5224c0bf5e963023ec62256
Heyucheng Zhang <heyucheng.zhang@mail.utoronto.ca>
2024-03-05 20:02:14 -0500

    add model

--- scripts/04-model.R
+++ scripts/04-model.R
@@ -11,21 +11,27 @@
 #### Workspace setup ####
 library(tidyverse)
 library(rstanarm)
+library(arrow)
 
 #### Read data ####
-analysis_data <- read_csv("outputs/data/analysis_data.csv")
+analysis_data <- read_parquet("outputs/data/analysis_data.parquet")
+set.seed(853)
+ces2020_reduced <- 
+  ces2020 |> 
+  slice_sample(n = 1000)
 
 ### Model data ####
 first_model <-
   stan_glm(
-    formula = flying_time ~ length + width,
-    data = analysis_data,
-    family = gaussian(),
+    voted_for ~ gender + education,
+    data = ces2020_reduced,
+    family = binomial(link = "logit"),
     prior = normal(location = 0, scale = 2.5, autoscale = TRUE),
-    prior_intercept = normal(location = 0, scale = 2.5, autoscale = TRUE),
-    prior_aux = exponential(rate = 1, autoscale = TRUE),
+    prior_intercept = 
+      normal(location = 0, scale = 2.5, autoscale = TRUE),
     seed = 853
   )
+
 
 
 #### Save model ####
